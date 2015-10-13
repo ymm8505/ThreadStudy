28c992384c3ffff7c644e79b24240f1b550c5896
ymm <@10.0.108.99>
2015-10-13 18:05:01 +0800

    空中网 面试题一

---
+++ ThreadStudy/src/Producer.java
@@ -0,0 +1,62 @@
+import java.util.concurrent.BlockingQueue;
+
+/*
+ * BlockingQueue 队列   实现生产者 消费者模式
+ * 
+ * */
+class Producer implements Runnable {
+	private final BlockingQueue queue;
+
+	Producer(BlockingQueue q) {
+		queue = q;
+	}
+
+	public void run() {
+	     try {
+	       while (true) { 
+	    	   queue.put(produce()); 
+	    	   }
+	     } catch (InterruptedException ex) { 
+//	    	 ... handle ...
+	    	 ex.printStackTrace();
+	    	 }
+	}	
+	
+	Object produce() {
+		System.out.println("我是 produce");
+		return "";
+		}
+}
+
+class Consumer implements Runnable {
+	private final BlockingQueue queue;
+
+	Consumer(BlockingQueue q) {
+		queue = q;
+	}
+
+	public void run() {
+     try {
+       while (true) { consume(queue.take()); }
+     } catch (InterruptedException ex) { 	
+    	 ex.printStackTrace();
+//    	 ... handle ...
+     }
+   }
+
+	void consume(Object x) {
+//		... 
+		}
+}
+
+class Setup {
+	void main() {
+		BlockingQueue q = new SomeQueueImplementation();
+		Producer p = new Producer(q);
+		Consumer c1 = new Consumer(q);
+		Consumer c2 = new Consumer(q);
+		new Thread(p).start();
+		new Thread(c1).start();
+		new Thread(c2).start();
+	}
+}
---
+++ ThreadStudy/src/read/Test.java
@@ -0,0 +1,70 @@
+package read;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/*第一题：现有的程序代码模拟产生了16个日志对象，并且需要运行16秒才能打印完这些日志，
+ * 请在程序中增加4个线程去调用parseLog()方法来分头打印这16个日志对象，
+ * 程序只需要运行4秒即可打印完这些日志对象。原始代码如下：
+ * 
+ * 
+ * 
+ * 分析思路
+*/
+	public class Test {
+		
+		public static void main(String[] args) throws InterruptedException{
+			
+			//add by yangmm 2015年10月13日11:42:40
+			/*for(int i =0 ;i<4;i++){
+				new Thread(new Runnable() {
+					@Override
+					public void run() {
+						
+					}
+				}).start();
+			}*/
+			
+			
+			/*final BlockingQueue<String> queue = new ArrayBlockingQueue<String>(1);
+			for(int i=0;i<4;i++){
+				new Thread(new Runnable(){
+					@Override
+					public void run() {
+						while(true){
+							try {
+								String log = queue.take();
+								parseLog(Thread.currentThread().getName()+log);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						}
+					}
+					
+				}).start();
+			}*/
+			
+			System.out.println("begin:"+(System.currentTimeMillis()/1000));
+			/*模拟处理16行日志，下面的代码产生了16个日志对象，当前代码需要运行16秒才能打印完这些日志。
+			修改程序代码，开四个线程让这16个对象在4秒钟打完。
+			*/
+			for(int i=0;i<16;i++){  //这行代码不能改动
+				final String log = ""+(i+1);//这行代码不能改动
+				{
+		    						Test.parseLog(log);
+//		    						queue.put(log);
+		    			}
+				}
+			}
+		
+		//parseLog方法内部的代码不能改动
+		public static void parseLog(String log){
+			System.out.println(log+":"+(System.currentTimeMillis()/1000));
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}		
+		}
+		
+	}
